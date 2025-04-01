f9ff7fa8a5fc2b709080fc2b979e6c69f9039e76
Peterson Germain <e2124621@cmaisonneuve.qc.ca>
2025-04-01 12:11:41 -0400

    page mobile terminée

--- 404.php
+++ 404.php
@@ -12,21 +12,23 @@
 
 <section class="erreur" style="background-image: url(<?php echo $erreur_background = get_theme_mod("erreur_background", "Default Title");?> );">
   <h1 class="erreur__titre" style="color: <?php echo $erreurTitre_couleur = get_theme_mod("erreurTitre_couleur", "#fff");?> ">Oops, vous avez échoué sur l'île 404 !</h1>
-  <p class="erreur__description"><?php echo $erreur_description = get_theme_mod("erreur_description", "Default Title");?> </p>
-  <div class="erreur__icones">
+  <p class="erreur__description" style="color: <?php echo $erreurTitre_couleur = get_theme_mod("erreurTitre_couleur", "#fff");?> "><?php echo $erreur_description = get_theme_mod("erreur_description", "Default Title");?> </p>
+  <!-- <div class="erreur__icones">
     <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="40" height="40">
     <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="40" height="40">
     <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="40" height="40">
     <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="40" height="40">
-  </div>
-  <p class="erreur__bouton">Retour à l'accueil</p>
+  </div> -->
+  <a href="https://gftnth00.mywhc.ca/4w4_33/" class="erreur__bouton">Retour à l'accueil</p>
   <?php wp_nav_menu(array(
     "menu" => "destinations",
     "container" => "nav",
     "container_class" => "erreur__destinations"
     )); ?>
 
-  <div class="erreur__recherche"><?php get_search_form();   ?></div>
+  <div class="erreur__recherche">
+  <?php get_search_form();   ?>
+  </div>
 </section>
 
 <?php get_footer(); ?>
