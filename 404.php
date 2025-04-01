107f64673630a111868b99e3f7364e9c41c4dc2e
Peterson Germain <e2124621@cmaisonneuve.qc.ca>
2025-04-01 10:08:22 -0400

    ajout de l'image background dans le customizer

--- 404.php
+++ 404.php
@@ -6,11 +6,12 @@
 ?>
 <?php $erreur_description = get_theme_mod("erreur_description", "Default Title");?>
 <?php $erreurTitre_couleur = get_theme_mod("erreurTitre_couleur", "#fff");?> 
+<?php $erreur_background = get_theme_mod("erreur_background", "Default Title");?> 
 
 <?php get_header() ?>
 
-<section class="erreur">
-  <h1 class="erreur__titre" style="color: <?php echo $erreurTitre_couleur = get_theme_mod("erreurTitre_couleur", "#fff");?> ">Erreur 404</h1>
+<section class="erreur" style="background-image: url(<?php echo $erreur_background = get_theme_mod("erreur_background", "Default Title");?> );">
+  <h1 class="erreur__titre" style="color: <?php echo $erreurTitre_couleur = get_theme_mod("erreurTitre_couleur", "#fff");?> ">Oops, vous avez échoué sur l'île 404 !</h1>
   <p class="erreur__description"><?php echo $erreur_description = get_theme_mod("erreur_description", "Default Title");?> </p>
   <div class="erreur__icones">
     <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="40" height="40">
@@ -18,12 +19,14 @@
     <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="40" height="40">
     <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="40" height="40">
   </div>
-  <p class="erreur__recommandation">Cherchez-vous plutôt : </p>
+  <p class="erreur__bouton">Retour à l'accueil</p>
   <?php wp_nav_menu(array(
     "menu" => "destinations",
     "container" => "nav",
     "container_class" => "erreur__destinations"
     )); ?>
+
+  <div class="erreur__recherche"><?php get_search_form();   ?></div>
 </section>
 
 <?php get_footer(); ?>
--- functions/customizer.php
+++ functions/customizer.php
@@ -131,6 +131,17 @@ function theme_31w_customize_register($wp_customize) {
     'section' => 'erreur_section',
     'type' => 'text',
   ));
+
+  /////////////////////////////////////////////////////////// Image en background de la zone erreur
+  $wp_customize->add_setting('erreur_background', array(
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+  
+  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'erreur_background', array(
+    'label' => __('Image en background', 'theme_31w'),
+    'section' => 'erreur_section',
+  )));
 }
 
 
