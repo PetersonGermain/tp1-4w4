7ae5498116842cb21123b6880ec87718bb6942ed
Peterson Germain <e2124621@cmaisonneuve.qc.ca>
2025-05-23 09:24:00 -0400

    ajout du template page pays, du titre et de l'introduction

---
+++ template-pays.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  Template Name: Modele Pays
+ */
+?>
+<?php get_header() ?>
+  <h1>template-pays.php</h1>
+  <section class="titre">
+    <h2>Les plus beau pays</h2>
+    <p>Plongez au cœur de l’aventure et laissez-vous emporter par l’appel du large ! Notre planète regorge de destinations incroyables, chacune promettant une expérience unique et mémorable. Que vous rêviez de plages idylliques baignées de soleil, de sommets majestueux invitant à la randonnée, de villes vibrantes d’histoire et de modernité, ou de rencontres culturelles authentiques, il y a un pays fait pour vous.</p>
+
+     <?php get_footer(); ?>
+  </section>
